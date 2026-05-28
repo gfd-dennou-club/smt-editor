2ff66bb199a11e7f2bc3099d1af51242f9cffce9
Ko-ichiro Sugiyama <sugiyama@gfd-dennou.org>
2026-05-28 23:22:58 +0900

    add symbol duty for pwm_init

--- packages/scratch-gui/src/lib/ruby-to-blocks-converter/smt_pwm.js
+++ packages/scratch-gui/src/lib/ruby-to-blocks-converter/smt_pwm.js
@@ -10,12 +10,15 @@ const SmT_PWM_Converter = {
 	
         // --- 代入 ($pwm1 = PWM.new) ---
         converter.registerOnVasgn((scope, variable, rh) => {
-            if (rh?.opcode !== "unifiedapi_pwm_init") return null;
+
+	    if (rh?.opcode !== "unifiedapi_pwm_init") return null;
 
             const varName = Utils.attachVariableBlock(converter, rh, "INSTANCE", variable);
-
+	    console.log( varName );
+	    
             converter._instanceTypeMap[varName] = "PWM";
             return rh;
+
         });
 
         // --- 初期化 (PWM.new) ---
@@ -44,7 +47,8 @@ const SmT_PWM_Converter = {
 		
                 // 第 2 引数のハッシュ (キーワード引数 timer:, frequency:) から値を取得
                 const timerNode = args[1]?.get("sym:timer");
-                const freqNode = args[1]?.get("sym:frequency");
+                const freqNode  = args[1]?.get("sym:frequency");
+                const dutyNode  = args[1]?.get("sym:duty");
 
                 // TIMER は正の整数
                 if (timerNode && timerNode.type === "int" && Number(timerNode.value) >= 0) {
@@ -59,9 +63,17 @@ const SmT_PWM_Converter = {
                 } else {
                     throw new Error(Utils.getErrorMessage('ONLY_POSITIVE_INTEGER', 3));
                 }
-
+		
+                // DUTY は正の整数（0以上）
+                if (dutyNode && dutyNode.type === "int" && Number(dutyNode.value) >= 0) {
+                    converter.addNumberInput(block, "DUTY", "math_integer", Number(dutyNode.value), 0);
+                } else {
+                    throw new Error(Utils.getErrorMessage('ONLY_POSITIVE_INTEGER', 3));
+                }
+		
                 return block;
             }
+
             return null;
         });
 
@@ -70,7 +82,7 @@ const SmT_PWM_Converter = {
             converter.registerOnSend('variable', method, 1, (params) => {
                 const { receiver, node, args } = params;
                 const varName = receiver.fields?.VARIABLE?.value || "";
-
+		
                 if (!args[0]) return null;
 		
 		// varName が空（変数ブロックとして認識されていない）場合はエラー
@@ -79,7 +91,7 @@ const SmT_PWM_Converter = {
                 }
 
                 if (converter._instanceTypeMap[varName] !== "PWM") return null;
-
+		
                 let opcode;
                 let inputName;
                 
@@ -140,83 +152,3 @@ const SmT_PWM_Converter = {
 
 export default SmT_PWM_Converter;
 
-/*
-const SmT_PWM_Converter = {
-    register: function (converter) {
-        converter._instanceTypeMap = converter._instanceTypeMap || {};
-
-        // --- 代入 ($pwm1 = PWM.new) ---
-	// この関数に入ったことで左辺が変数であることが保証されている
-        converter.registerOnVasgn((scope, variable, rh) => {
-
-	    // 右辺が PWM.new ブロックでなければ無視
-	    if (rh?.opcode !== "unifiedapi_pwm_init") return null;
-
-	    //ブロック作成
-            const varName = Utils.attachVariableBlock(converter, rh, "INSTANCE", variable);
-            converter._instanceTypeMap[varName] = "PWM";
-            return rh;
-        });
-
-        // --- 初期化 (PWM.new) ---
-        converter.registerOnSend("::PWM", "new", 2, (params) => {
-            const { args, node } = params;
-            const block = converter.createBlock("unifiedapi_pwm_init", "statement", node);
-            
-            Utils.fixLocationToLineStart(block);
-	    //console.log( args );
-	    
-            if (block && converter.isNumber(args[0])) {
-                converter.addNumberInput(block, "PIN", "math_integer", Number(args[0].value), 39);
-		
-		const timer = Number(args[1]?.get("sym:timer").value);
-                converter.addNumberInput(block, "TIMER", "math_integer", timer, 0);
-                
-                const freq = Number(args[1]?.get("sym:frequency").value);
-                converter.addNumberInput(block, "FREQ", "math_integer", freq, 50);
-
-		return block;
-            }
-            return null;
-        });
-
-        // --- メソッド (.read, .read_raw) ---
-        ["duty", "frequency", "pulse_width_us"].forEach(method => {
-            converter.registerOnSend('variable', method, 1, (params) => {
-                const { receiver, node, args } = params;
-                const varName = receiver.fields?.VARIABLE?.value || "";
-
-                if (converter._instanceTypeMap[varName] !== "PWM") return null;
-
-		// メソッド名に対応する Opcode の決定
-                let opcode;
-                let inputName;
-                switch (method) {
-                    case "duty": 
-                        opcode = "unifiedapi_pwm_duty"; 
-                        inputName = "DUTY";
-                        break;
-                    case "frequency": 
-                        opcode = "unifiedapi_pwm_frequency"; 
-                        inputName = "FREQ";
-                        break;
-                    case "pulse_width_us": 
-                        opcode = "unifiedapi_pwm_pulse"; 
-                        inputName = "PULSE";
-                        break;
-                }
-		
-                const block = converter.createBlock(opcode, "statement", node);
-                if (block) {
-                    Utils.attachVariableBlock(converter, block, "INSTANCE", receiver);
-		    converter.addNumberInput(block, inputName, "math_integer", Number(args[0].value), 0);
-                    return block;
-                }
-                return null;
-            });
-        });
-    }
-};
-
-export default SmT_PWM_Converter;
-*/
